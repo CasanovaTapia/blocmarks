a45d1f426528a232e12a37b79e06e36097b0c998
CasanovaTapia <ching@casanovatapia.com>
2014-10-22 13:16:00 +0800

    Comment out Incoming Controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,26 +1,26 @@
-class IncomingController < ApplicationController
-
-  skip_before_filter :verify_authenticity_token, only: [:create]
-
-  puts "INCOMING PARAMS HERE: #{params}"
-
-  def create
-    sender = params['sender']
-    subject = params['subject']
-    body = params['body']
-    user = User.find_by_email(sender)
-    id = user.id
-    email = user.email
-
-    bookmark = user.bookmarks.create( url: body )
-
-    hashtags = params[:subject].scan(/#\w+/)
-    hashtags.each do |hashtag|
-      hashtag.sub!('#', '')
-      category = Category.find_or_create_by(name: hashtag)
-      bookmark.categories << category
-    end
-
-    head 200
-  end
-end
+# class IncomingController < ApplicationController
+#
+#   skip_before_filter :verify_authenticity_token, only: [:create]
+#
+#   puts "INCOMING PARAMS HERE: #{params}"
+#
+#   def create
+#     sender = params['sender']
+#     subject = params['subject']
+#     body = params['body']
+#     user = User.find_by_email(sender)
+#     id = user.id
+#     email = user.email
+#
+#     bookmark = user.bookmarks.create( url: body )
+#
+#     hashtags = params[:subject].scan(/#\w+/)
+#     hashtags.each do |hashtag|
+#       hashtag.sub!('#', '')
+#       category = Category.find_or_create_by(name: hashtag)
+#       bookmark.categories << category
+#     end
+#
+#     head 200
+#   end
+# end
